ba286ce7c01ccb9865b2d6e3ceae7aa93c7debea
Abdulkadir Eroglu <ae.92736@gmail.com>
2022-11-11 11:44:03 +0300

    Some test on the rust structure

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,11 @@
 fn main() 
 {
-    println!("Hello, world!");
+    let x: f32 = 256.0;
+    println!("x is {}", x);
+    {
+        let x: i32 = x as i32 - 2;
+        println!("x is {}", x);
+    }
+    let x: i128 = 42;
+    println!("x is {}", x);
 }
